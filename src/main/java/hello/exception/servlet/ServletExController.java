c32b855927dff206e47c774a4c431f8869a87272
f1v3-dev <f1v3_dev@naver.com>
2023-07-15 11:11:50 +0900

    [서블릿 예외 처리 - 오류 화면 제공]

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -21,6 +21,7 @@ public class ServletExController {
         response.sendError(404, "404 오류!");
     }
 
+
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500, "404 오류!");
